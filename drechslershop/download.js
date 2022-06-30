d8ef0578bb5a4199086c63fb4964064574661fb5
bn1 <bn1>
2022-06-30 21:53:56 +0200

    drechslershop: init

--- drechslershop/download.js
+++ drechslershop/download.js
@@ -1,3 +1,63 @@
+const Apify = require("apify");
+const axios = require("axios");
+const {ungzip} = require("node-gzip");
+const cheerio = require("cheerio");
 
 
-module.exports = async () => {}
+const {log} = Apify.utils;
+
+
+module.exports = async () => {
+    log.debug('drechslershop.download');
+
+    let drechslershop_pages = await Apify.openDataset('drechslershop-pages')
+    let drechslershop_pages_info = await drechslershop_pages.getInfo();
+
+    if (drechslershop_pages_info.itemCount) {
+        log.debug('drechslershop.download - skipped');
+        return
+    }
+
+    let requestQueue = await Apify.openRequestQueue('drechslershop');
+    let sitemap = 'https://drechslershop.de/web/sitemap/shop-1/sitemap-1.xml.gz';
+
+    sitemap = await axios.get(sitemap, {responseType: 'arraybuffer'});
+    sitemap = (await ungzip(sitemap.data)).toString();
+
+    let $ = cheerio.load(sitemap);
+    let urls = $('url loc').map((index, element) => $(element).text()).toArray();
+
+    for (let url of urls) {
+        await requestQueue.addRequest({url})
+    }
+
+    const crawler = new Apify.CheerioCrawler({
+        requestQueue,
+
+        minConcurrency: 1,
+        maxConcurrency: 10,
+        // minConcurrency: 5,
+        // maxConcurrency: 10,
+
+        maxRequestRetries: 10,
+
+        handlePageTimeoutSecs: 30,
+
+        maxRequestsPerCrawl: 5000,
+
+        handlePageFunction: async ({request, $}) => {
+            log.debug(`Processing ${request.url}...`);
+
+            await drechslershop_pages.pushData({html: $.html()});
+        },
+
+        // This function is called if the page processing failed more than maxRequestRetries+1 times.
+        handleFailedRequestFunction: async ({ request }) => {
+            await bamato_failed.addRequest(request);
+            log.debug(`Request ${request.url} is failing.`);
+        },
+    });
+
+    await crawler.run();
+    log.debug('drechslershop.download - done');
+}
--- drechslershop/parse.js
+++ drechslershop/parse.js
@@ -1,3 +1,36 @@
+const Apify = require("apify");
+const cheerio = require("cheerio");
 
 
-module.exports = async () => {}
+const {log} = Apify.utils;
+
+
+module.exports = async () => {
+    log.debug('drechslershop.parse');
+
+    log.debug('drechslershop.parse - skipping');
+    return;
+
+    let drechslershop_pages = await Apify.openDataset('drechslershop-pages');
+    let drechslershop_pages_info = await drechslershop_pages.getInfo();
+
+    let counter = 0;
+    let suppliers = {};
+
+    await drechslershop_pages.forEach((page, index) => {
+        let $ = cheerio.load(page.html);
+
+        let supplier = $('.product--supplier a')[0];
+        if (supplier) {
+            supplier = supplier.attribs.title;
+        }
+        if (!suppliers[supplier]) {
+            suppliers[supplier] = 0;
+        }
+        suppliers[supplier]++;
+
+        log.debug(`drechslershop.parse - ${ counter } (${ index }/${ drechslershop_pages_info.itemCount })`);
+    })
+
+    log.debug('drechslershop.parse - done');
+}
